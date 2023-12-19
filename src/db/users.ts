68903b93242e36911d6a1e20b67260f390d8b83d
11geraldmoney <11geraldmoney@gmail.com>
2023-12-19 13:58:32 +0300

    build: project

---
+++ src/db/users.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+/** user */
+const userSchema = new mongoose.Schema({
+  username: { type: "string", required: true },
+  email: { type: "string", required: true },
+
+  authentication: {
+    password: { type: "string", required: true, select: false },
+    salt: { type: "string", select: false },
+    sessionToken: { type: "string", select: false },
+  },
+});
