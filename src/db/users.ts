7bbc24dcd65f4c0f9f25f455dcab1955ba9ced2a
11geraldmoney <11geraldmoney@gmail.com>
2023-12-20 15:18:33 +0300

    feat: building user model & schema

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,3 +1,4 @@
+import { FindOperators } from "mongodb";
 import mongoose from "mongoose";
 
 /** user */
@@ -7,9 +8,31 @@ const userSchema = new mongoose.Schema({
 
   authentication: {
     password: { type: "string", required: true, select: false },
+    //salt is used to prevent from rainbow table attacks
     salt: { type: "string", select: false },
     sessionToken: { type: "string", select: false },
   },
 });
 
+const UserModel = mongoose.model("User", userSchema);
 
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserById = (id: string) => UserModel.findById(id);
+
+/** Authentication */
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({ "authentication.sessionToken": sessionToken });
+
+/** Creating a user */
+export const createUser = (values: Record<string, any>): Promise<any> =>
+  new UserModel(values).save().then((user) => user.toObject());
+
+/** Delete user */
+export const deleteUserById = (id: string) =>
+  UserModel.findOneAndDelete({ _id: id });
+
+export const updateUserById = (
+  id: string,
+  values: Record<string, any>
+): Promise<any> => UserModel.findByIdAndUpdate(id, values);
